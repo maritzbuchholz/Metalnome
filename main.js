74774d17d8568d10e49a09f1f36642a3bbbdaad6
maritzbuchholz <maritzbuchholz@gmail.com>
2025-11-29 13:00:08 -0500

    add tone.js

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 const bpm = document.getElementById("bpm");
 const start = document.querySelector(".metronome__start");
+const synth = new Tone.Synth().toDestination(); //create a synth and connect it to the main output (your speakers)
 
-start.addEventListener("click", function(event) {
+start.addEventListener("click", async (event) => {
+    event.preventDefault();
+    await Tone.start();
     console.log(bpm.value);
+    synth.triggerAttackRelease("D4", "16n");
 });
