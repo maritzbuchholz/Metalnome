7fbfc0f8040c59eedb13d65c9376da0bf9acd952
maritzbuchholz <maritzbuchholz@gmail.com>
2025-11-29 14:12:36 -0500

    add pause functionality to metronome

--- main.js
+++ main.js
@@ -2,21 +2,32 @@
 
 const start = document.querySelector(".metronome__start");
 
-function startBasic () {
+function basicStart () {
     const bpm = document.getElementById("bpm");
     const synth = new Tone.Synth().toDestination();
     Tone.Transport.bpm.value = bpm.value;
     const metroLoop = new Tone.Loop(
         (time) => {
-            synth.triggerAttackRelease("C4", "8n", time);
+            synth.triggerAttackRelease("G#4", "8n", time);
             console.log("click");
-        },"4n"
+        },"8n"
     ).start(0);
     Tone.Transport.start();
 }
 
+function stop() {
+    Tone.Transport.stop();
+    Tone.Transport.cancel(); // Clears all scheduled events
+}
+
+let metronomeOn = false;
 start.addEventListener("click", async (event) => {
     event.preventDefault();
-    await Tone.start();
-    startBasic();
+    metronomeOn = !metronomeOn;
+    if (metronomeOn){
+        await Tone.start();
+        basicStart();
+    } else {
+        stop();
+    }
 });
