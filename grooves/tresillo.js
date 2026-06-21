125b42756caba375d3021b8aa81b6197a2f4fca5
maritzbuchholz <maritzbuchholz@gmail.com>
2026-06-20 23:12:08 -0400

    update tresillo beat to ride on the bass drum

--- grooves/tresillo.js
+++ grooves/tresillo.js
@@ -8,21 +8,39 @@ function tresillo(metalnomeBoolean) {
   const synth = new Tone.Synth().toDestination();
   Tone.Transport.bpm.value = bpm.value;
   const timeKeeperCall = timeKeeper(metalnomeBoolean);
-  const seq = new Tone.Sequence(
-    (time, note) => {
-      synth.triggerAttackRelease(note, "16n", time);
-    },
-    ["G#4", null, null, "G#4", null, null, "G#4", null],
-    "16n"
-  ).start(0);
+  // const seq = new Tone.Sequence(
+  //   (time, note) => {
+  //     synth.triggerAttackRelease(note, "16n", time);
+  //   },
+  //   ["G#4", null, "G#4", "G#4", null, null, "G#4", null],
+  //   "8n"
+  // ).start(0);
   
-  const drum = new Tone.Sequence(
+  const closedhh = new Tone.Sequence(
     (time, note) => {
       players.player(note).start(time);
     },
-    ["kick", null, null, null, "snare", null, null, null],
+    ["closedhh", null, "closedhh", null, "closedhh", null, "closedhh", null],
     "8n"
   ).start(0);
+
+  
+  const snare = new Tone.Sequence(
+    (time, note) => {
+      players.player(note).start(time);
+    },
+    [null, null, null, null, "snare", null, null, null],
+    "8n"
+  ).start(0);
+
+  const kick = new Tone.Sequence(
+    (time, note) => {
+      players.player(note).start(time);
+    },
+    ["kick", null, null, "kick", null, null, "kick", null],
+    "16n"
+  ).start(0);
+
   Tone.Transport.start();
 }
 
